953dc9b4c63c7eac7366cd0fa095e0fba229315b
Khalid Hasan <sawmaster22@gmail.com>
2023-12-13 17:39:11 +0700

    add[feat]: finalize snow boarding game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float defaultTorqueAmount = 0.3f;
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+
+    SurfaceEffector2D surfaceEffector2D;
     Rigidbody2D rigidbody2d;
+    bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>(); 
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if(canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void DisableControls()
+    {
+        canMove = false;
+    }
+
+    void RotatePlayer()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
@@ -23,6 +44,19 @@ public class PlayerController : MonoBehaviour
         else if(Input.GetKey(KeyCode.RightArrow))
         {
             rigidbody2d.AddTorque(-torqueAmount);
+        }
+        rigidbody2d.AddTorque(-defaultTorqueAmount);
+    }
+
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
